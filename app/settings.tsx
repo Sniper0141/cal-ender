d243c4ca2bc5afa64db3415fc1ff0d2ce0aebf94
Sniper0141 <116571645+Sniper0141@users.noreply.github.com>
2025-04-08 11:32:11 +0200

    settings page done

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,16 +1,31 @@
-import { useEffect } from 'react';
-import { StyleSheet, Text, View } from "react-native";
-import { Link, useRouter } from "expo-router";
+import { useState } from "react";
+import { StyleSheet, Text, View, Switch } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Settings() {
+
     const router = useRouter();
   
+    const [vibrationOn, setVibrationOn] = useState(readSettings()); // read backend for inital state of button
+    
+
+    function readSettings() {
+        // Read
+        // return boolean
+        return false;
+    }
+
+    function toggleVibrationOn(){
+        setVibrationOn(previous => !previous);
+    }
 
     return (
-        <View
-        style={styles.main}
-        >
-            <Text onPress={() => router.back()}>In the settings</Text>
+        <View style={styles.main}>
+            <Text style={styles.backButton} onPress={() => router.back()}>← Back</Text>
+            <View style={styles.settingsSection}>
+                <Text style={styles.settingsText}>Haptic Feedback</Text>
+                <Switch onValueChange={toggleVibrationOn} value={vibrationOn}></Switch>
+            </View>
         </View>
     );
 }
@@ -18,7 +33,23 @@ export default function Settings() {
 const styles = StyleSheet.create({
     main:{
         flex: 1,
-        alignItems: "center",
-
+        backgroundColor: "#000000"
     },
+    backButton:{
+        marginTop: 15,
+        marginLeft: 20,
+        alignSelf: "flex-start",
+        fontSize: 24,
+        color: "#C800FA"
+    },
+    settingsSection:{
+        flexDirection: "row",
+        gap: 20,
+        marginTop: 40,
+        marginLeft: 20,
+    },
+    settingsText:{
+        color: "#FFFFFF",
+        fontSize: 24
+    }
 });
