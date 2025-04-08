0d24d186eeae71c37bada03905d181c08905ca29
Sniper0141 <116571645+Sniper0141@users.noreply.github.com>
2025-04-08 14:23:24 +0200

    calendar works

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,37 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { Calendar } from 'react-native-calendars';
 
 
 export default function Index() {
+
+  const [selectedDay, setSelectedDay] = useState("");
+
   return (
     <View style={styles.main}>
       <Link style={styles.settingsButton} href="/settings">Settings</Link>
+      <Calendar
+        onDayPress={(day: any) => {
+          setSelectedDay(day.dateString)
+        }}
+        markedDates={{
+          [selectedDay]: {selected: true }
+        }}
+        theme={{
+          backgroundColor: '#000000',
+          calendarBackground: '#000000',
+          textSectionTitleColor: '#C800FA',
+          selectedDayBackgroundColor: '#FFFFFF',
+          selectedDayTextColor: '#000000',
+          todayBackgroundColor: "#DE7AFA",
+          todayTextColor: '#000000',
+          dayTextColor: '#ffffff',
+          monthTextColor: '#ffffff',
+          textDisabledColor: '#2d4150'
+        }}
+        style={styles.calendar}
+      />
     </View>
   );
 }
@@ -24,5 +48,9 @@ const styles = StyleSheet.create({
     marginTop: 15,
     marginRight: 20,
     color: "#C800FA"
+  },
+  calendar:{
+    transform: [{ scale: 1.2 }],
+    marginTop: 50
   }
 });
